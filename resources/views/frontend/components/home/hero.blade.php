b0c7104caf82d91d5a4baee37a750b2f202ab457
gracychadha <chadhagracy8@gmail.com>
2026-07-06 11:59:34 +0530

    cta updtae

--- resources/views/frontend/components/home/hero.blade.php
+++ resources/views/frontend/components/home/hero.blade.php
@@ -8,8 +8,12 @@
             <div class="col-12 p-0">
 
                 <div class="rx-slider">
+                    @php
+                        $generalSetting = App\Models\GeneralSetting::first();
+                    @endphp
                     @forelse($sliders as $slider)
-                        <div class="rx-slide slide-{{ $loop->iteration }}"   style="background-image: url('{{ asset("storage/" . $slider->image) }}'); background-size: cover; background-position: center; background-repeat: no-repeat;">
+                        <div class="rx-slide slide-{{ $loop->iteration }}"
+                            style="background-image: url('{{ asset("storage/" . $slider->image) }}'); background-size: cover; background-position: center; background-repeat: no-repeat;">
                             <img src="{{ asset('storage/' . $slider->image) }}" alt="{{ $slider->title }}"
                                 class="banner-arrow-img">
 
@@ -30,7 +34,8 @@
 
                                                     <div class="booking-text">
                                                         <p>Book Now</p>
-                                                        <span>987 654 3210</span>
+                                                        <span><a class="text-white"
+                                                                href="tel:{{ $generalSetting->phone }}" target="_blank">{{ $generalSetting->phone }}</a></span>
                                                     </div>
                                                 </div>
 
@@ -57,7 +62,8 @@
                                                     </div>
                                                     <div class="booking-text">
                                                         <p>Book Now</p>
-                                                        <span>987 654 3210</span>
+                                                        <span><a class="text-white"
+                                                                href="tel:{{ $generalSetting->phone }}" target="_blank">{{ $generalSetting->phone }}</a></span>
                                                     </div>
                                                 </div>
                                             </div>
@@ -66,56 +72,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="rx-slide slide-2">
-                            <img src="assets/img/hero/box-hero-2.png" alt="hero-box" class="banner-arrow-img">
-                            <div class="rx-hero-contact">
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col-12">
-                                            <div class="inner-contact slider-animation">
-                                                <p>Luxury Hotel & Restaurant</p>
-                                                <h2>Enjoy Your <span>Dream</span> Time With <span>Luxury</span>
-                                                    Experience</h2>
-                                                <div class="booking-now">
-                                                    <div class="ico">
-                                                        <i class="ri-phone-line"></i>
-                                                    </div>
-                                                    <div class="booking-text">
-                                                        <p>Book Now</p>
-                                                        <span>987 654 3210</span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="rx-slide slide-3">
-                            <img src="assets/img/hero/box-hero-3.png" alt="hero-box" class="banner-arrow-img">
-                            <div class="rx-hero-contact">
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col-12">
-                                            <div class="inner-contact slider-animation">
-                                                <p>Luxury Hotel & Restaurant</p>
-                                                <h2>Enjoy Your <span>Dream</span> Time With <span>Luxury</span>
-                                                    Experience</h2>
-                                                <div class="booking-now">
-                                                    <div class="ico">
-                                                        <i class="ri-phone-line"></i>
-                                                    </div>
-                                                    <div class="booking-text">
-                                                        <p>Book Now</p>
-                                                        <span>987 654 3210</span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+
                     @endforelse
                 </div>
 
