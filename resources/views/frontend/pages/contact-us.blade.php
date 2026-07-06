ec6acbd721f36fb19413bc267bdb7c4a384b1ee7
gracychadha <chadhagracy8@gmail.com>
2026-07-06 11:38:25 +0530

    zp map

--- resources/views/frontend/pages/contact-us.blade.php
+++ resources/views/frontend/pages/contact-us.blade.php
@@ -53,12 +53,21 @@
                         <div class="row mb-minus-24">
                             <div class="col-lg-6 col-12 mb-24">
                                 <div class="rx-contact-touch-ifrem">
-                                    @php
-                                        $generalSetting = App\Models\GeneralSetting::first();
-                                    @endphp
-                                    <iframe
-                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29749.225774882685!2d72.84343101893258!3d21.245595574425934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04f4fb5c0b087%3A0xb7aabd8a90da0679!2sMota%20Varachha%2C%20Surat%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1690017805909!5m2!1sen!2sin"></iframe>
-                                </div>
+                                  
+                                @php
+    $generalSetting = App\Models\GeneralSetting::first();
+@endphp
+
+<iframe
+    width="100%"
+    height="450"
+    style="border:0;"
+    loading="lazy"
+    allowfullscreen
+    referrerpolicy="no-referrer-when-downgrade"
+    src="https://www.google.com/maps?q={{ urlencode($generalSetting->address) }}&output=embed">
+</iframe>
+                                  </div>
                             </div>
                             <div class="col-lg-6 col-12 mb-24">
                                 <div class="rx-inner-form">
